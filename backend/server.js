177d13eb36d0206f464764ba7b5bd6c97dbf659d
Dharana418 <thiyunuwan567@gmail.com>
2025-09-27 01:27:42 +0530

    w

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+//connect to express app
+const app = express();
+
+//connect to MongoDB
+
+
+//middlewares
+
+
+//schemas
+
+
+//routes
+
+app.listen(3001)
+
+
